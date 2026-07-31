cebc9579da97d070d745d0805c6bc76827f6bd75
ngeethika15 <ngeethika.15@gmail.com>
2026-07-31 13:30:55 +0530

    Update footer copyright name to Neelam Geethika

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer(){
     return(
         <footer className='site-footer'>
             <p>
-        &copy;{year} Rakesh K R. Built with React🌐
+        &copy;{year} Neelam Geethika. Built with React🌐
     </p>
         </footer>
     )
